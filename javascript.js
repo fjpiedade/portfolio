99e6dced71e9d85389e447dbcac34a7aaec7269d
fjpiedade <fernandp.joao@gmail.com>
2023-07-25 13:15:40 +0100

    final change add animations and whatsapp button

---
+++ javascript.js
@@ -0,0 +1,11 @@
+const indicator = document.querySelector('#indicator');
+const maxHeight = document.body.scrollHeight - window.innerHeight;
+
+window.addEventListener('scroll', () => {
+  const percentage = (window.scrollY / maxHeight) * 100;
+  indicator.style.width = `${percentage}%`;
+});
+
+
+AOS.init();
+
